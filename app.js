36faf431da954155e5d50fea05391098f0a3e589
Jasmine <jasmineoliver158@gmail.com>
2026-03-20 15:31:33 -0400

    Initial commit

---
+++ app.js
@@ -0,0 +1,35 @@
+let taskArray = [];
+let categories = [];
+let addbtn = document.getElementById('addTask');
+let list = document.getElementById('list');
+
+
+
+addbtn.addEventListener("click", function () {
+let task = {
+    name: document.getElementById('name').value,
+    category: document.getElementById('category').value,
+    deadline: document.getElementById('deadline"').value,
+    status: "In Progress"
+}
+
+    taskArray.push(task);   
+    console.log(taskArray);
+    renderTask();
+})
+
+function renderTask() {
+    list.innerHTML = "";
+    for (let i = 0; i < taskArray.length; i++) {
+        let taskItem = document.createElement("li");
+        // taskItem.innerText = 
+        // taskArray[i].name + "-" +
+        // taskArray[i].category + "-" +
+        // taskArray[i].status;
+        console.log("hi");
+console.log(taskArray[i]);
+        // list.appendChild(taskItem);
+    }
+
+}
+
